c81915c6df555e0e1316b162910bd683f49041ea
dik82 <dae02091982@gmail.com>
2021-09-01 14:12:08 +0300

    init2

--- products/views.py
+++ products/views.py
@@ -7,3 +7,23 @@ def index(request):
 
 def products(request):
     return render(request, 'products.html')
+
+
+def test(request):
+    context = {
+        'title': 'geekshop',
+        'user': 'Ivanov',
+        'description': 'Добро пожаловать в Geekshop!',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals','price': '6 090,00' },
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00'},
+    ],
+    'promotion': True,
+    'products_of_promotion': [
+        #{'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
+        #{'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
+        ]
+    }
+
+    return render(request, 'test.html', context)

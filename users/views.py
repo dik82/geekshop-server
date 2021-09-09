b3c76e0f943f7503be75dd5e2b4cf9acd11aec87
dik82 <dae02091982@gmail.com>
2021-09-09 16:22:29 +0300

    create register

--- users/views.py
+++ users/views.py
@@ -1,19 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
+from .forms import UserLoginForm, UserRegisterForm
 
 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
-        'title': 'GeekShop - Авторизация'
+        'title': 'GeekShop - Авторизация',
+        'form': form
     }
 
     return render(request, 'users/login.html', context)
 
 
 def register(request):
-    context = {
-        'title': 'GeekShop - Регистрация'
-    }
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            form.save()
 
-    return render(request, 'users/register.html', context)
+            return HttpResponseRedirect(reverse('products'))
+        else:
+            print(form.errors)
+
+    else:
+        form = UserRegisterForm()
+        context = {
+            'title': 'GeekShop - Регистрация',
+            'form': form
+        }
+
+        return render(request, 'users/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
